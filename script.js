372de3a4eeea590efd97c006c77155b2c19a2d12
reactnative741 <reactnative74@gmail.com>
2024-02-03 13:35:16 +0330

    Get Api

--- script.js
+++ script.js
@@ -4,7 +4,35 @@ function GetUsers() {
   xhr.onload = function () {
     if (this.status == 200) {
       var users = JSON.parse(this.responseText);
-      console.log(users);
+      for (const i in users) {
+        const li = document.createElement("li");
+        li.className = "col-md-4 testimonial";
+        const divRow = document.createElement("div");
+        divRow.className = "row";
+        li.append(divRow);
+        const divAvatar = document.createElement("div");
+        divAvatar.className = "avatar col-md-5";
+        divRow.append(divAvatar);
+        const img = document.createElement("img");
+        img.className = "img-circle";
+        img.setAttribute("src", users[i].avatar_url);
+        img.setAttribute("alt", users[i].login);
+        divAvatar.append(img);
+        const divTestMain = document.createElement("div");
+        divTestMain.className = "testimonial-main col-md-7";
+        divAvatar.after(divTestMain);
+        const h4 = document.createElement("h4");
+        h4.className = "media-heading";
+        h4.innerText = users[i].login;
+        h4.style.color = "#23527c";
+        divTestMain.append(h4);
+        const p = document.createElement("p");
+        p.className = "testimony-body";
+        p.innerText = users[i].node_id;
+        h4.after(p);
+
+        document.querySelector("#list-group").append(li);
+      }
     }
   };
   xhr.send();
